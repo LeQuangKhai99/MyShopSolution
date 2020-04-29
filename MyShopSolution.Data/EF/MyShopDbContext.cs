14f8e320191236d936bc4ed6f935d0e68a1e3101
LeQuangKhai99 <fcxomben99@gmail.com>
2020-04-29 22:39:42 +0700

    Thêm các bảng

--- MyShopSolution.Data/EF/MyShopDbContext.cs
+++ MyShopSolution.Data/EF/MyShopDbContext.cs
@@ -15,5 +15,26 @@ namespace MyShopSolution.Data.EF
 
         public DbSet<Product> Products { set; get; }
         public DbSet<Category> Categories { set; get; }
+        public DbSet<AppConfig> AppConfigs { get; set; }
+
+
+        public DbSet<Cart> Carts { get; set; }
+
+        public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
+        public DbSet<ProductInCategory> ProductInCategories { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Language> Languages { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<ProductTranslation> ProductTranslations { get; set; }
+
+        public DbSet<Promotion> Promotions { get; set; }
+
+
+        public DbSet<Transaction> Transactions { get; set; }
     }
 }
