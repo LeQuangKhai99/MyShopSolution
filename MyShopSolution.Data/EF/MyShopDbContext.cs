4ad7de44f444ed088086ef2d3ece588185548bda
LeQuangKhai99 <fcxomben99@gmail.com>
2020-04-29 22:08:34 +0700

    Add Entity

---
+++ MyShopSolution.Data/EF/MyShopDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyShopSolution.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyShopSolution.Data.EF
+{
+    public class MyShopDbContext : DbContext
+    {
+        public MyShopDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Product> Products { set; get; }
+        public DbSet<Category> Categories { set; get; }
+    }
+}
---
+++ MyShopSolution.Data/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MyShopSolution.Data.Entities
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public decimal Price { get; set; }
+        public decimal PromotionPrice { get; set; }
+        public int Stock { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string SeoAlias { get; set; }
+    }
+}
